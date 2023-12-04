8f6e4c1eacc3cdca391c7608b08cbbedf7dbbf25
vp-1996 <119441482+vp-1996@users.noreply.github.com>
2023-12-04 22:09:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import studentRouter from './router/student.router.js';
+import studentRouter from './router/student.router.mjs';
 import dotenv from "dotenv";
 
 let app = express()
