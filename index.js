08099c9cf4beef07e92f74a18ba496392e4e7e11
vp-1996 <119441482+vp-1996@users.noreply.github.com>
2023-12-05 13:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json()) 
 
 var corsOptions = { 
-    origin: 'https://brilliant-travesseiro-ee174f.netlify.app/',
+    origin: 'https://brilliant-travesseiro-ee174f.netlify.app',
     optionsSuccessStatus: 200, // For legacy browser support
     methods: "GET, PUT, PATCH,  POST, DELETE"
 }
